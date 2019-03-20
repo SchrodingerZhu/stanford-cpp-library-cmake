05c5b95c07013586c6cc3b820c06a8acc685bbf5
SchrodingerZhu <118010469@link.cuhk.edu.cn>
2019-03-21 03:22:00 +0800

    small cleanup

--- src/hello.cpp
+++ src/hello.cpp
@@ -3,16 +3,22 @@
 #include <graphics/gwindow.h>
 #include <io/tokenscanner.h>
 #include <io/console.h>
+#include <collections/set.h>
 using namespace std;
 
 int main() {
     Vector<int> tt;
     TokenScanner tsc(cin);
     GWindow t;
+    Set<int> mm {};
     t.setSize(2000, 2000);
     t.setWindowTitle("123");
-    t.drawLine(0, 0, 500, 500);
-    char a = tsc.getChar();
+    t.drawLine(0, 0, 500, 800);
+    int a = tsc.getChar();
+    mm.insert(123);
+    for(auto i : mm) {
+        cout << i << std::endl;
+    }
     std::cout << a << std::endl;
     return 0;
 }
